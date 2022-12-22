ba172c3299427f022ed3c6f4bd01f4004d7da20e
Md.AlAmin <alamin.dreams2021@gmail.com>
2022-12-22 23:44:34 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,144 @@
+// // Comparisons operator
+
+// // equal to ==
+
+// console.log(10 == 10);
+// console.log(9 == "9");
+
+// //equal to value or data type ===
+
+// console.log(5 === "5");
+// console.log(5 === 5);
+
+// // not equal   !=
+
+// console.log(5 != 10);
+// console.log(10 != 10);
+
+// // not equal value or not equal data type  !==
+
+// console.log(10 !== "10");
+// console.log(10 !== 10);
+
+// // greather than >
+// console.log(10 > 9);
+// console.log(10 > 10);
+
+// // less than <
+
+// console.log(15 < 16);
+// console.log(15 < 12);
+
+// // greather than or equal to >=
+
+// console.log(10 >= 10);
+// console.log(10 >= 9);
+// console.log(10 >= 12);
+
+// // less than or equal to  <=
+
+// console.log(15 <= 14);
+// console.log(10 <= 15);
+
+// Logical Operator
+
+// 1. Logical And   &&
+// 2. Logical Or ||
+// 3. Logical Not  !
+
+// Logical and
+
+// let X = 5;
+// let y = 7;
+
+// console.log(X > 4);
+// console.log(y < 9);
+
+// console.log(X > 4 && y < 9);
+
+// Logical Or
+
+// let x = 7;
+// let y = 4;
+
+// console.log(x > 5);
+// console.log(y < 3);
+// console.log(x > 5 || y < 3);
+
+// logical not !
+
+// let x = 7;
+// let y = 4;
+
+// console.log(x > 5);
+// console.log(x == y);
+// console.log(!(x == y));
+
+// Operator Precidence
+//  B O D M A S
+
+// console.log((36 / 4) * 2 + 25 * 6 - 3 + (5 + 4));
+// // console.log((36 / 4) * 2 + 25 * 6 - 3 + 9);
+// // console.log((36 / 4) * 2 + 150 - 3 + 9);
+// // console.log(9*2+150-3+9);
+// // console.log(18+150-3+9);
+// // console.log(168-3+9);
+// console.log(177 - 3);
+// console.log(174);
+
+//  Template Literals
+
+// I am Shakhawat AlAmin,34 years old, web devoloper.
+
+// const firstName = "Shakhawat";
+// const lastName = "AlAmin";
+// let age = 34;
+// let job = "web devoloper";
+
+// // console.log(
+// //   " I am " +
+// //     firstName +
+// //     " " +
+// //     lastName +
+// //     "," +
+// //     age +
+// //     " years old" +
+// //     "," +
+// //     job +
+// //     "."
+// // );
+
+// console.log(` I am ${firstName} ${lastName}, ${age} years old, ${job}.`);
+
+// console.log(`I love my country Bangladesh,
+// Because This is My Birthplace
+// & Its the land of unique country.`);
+
+// // if else statement
+// if (10 < 5) {
+//   console.log("Display None");
+// } else {
+//   console.log("Display 10");
+// }
+
+// let fahimAge = 6;
+
+// if (fahimAge <= 6) {
+//   console.log("He can go to School");
+// }
+
+// if (fahimAge >= 6) {
+//   console.log("He can  go school");
+// } else {
+//   console.log("He can not go to School");
+// }
+
+// if (5 > 3) {
+//   console.log("getting display 5");
+// }
+
+// if (5 < 3) {
+//   console.log("getting display 5");
+// } else {
+//   console.log("getting display 3");
+// }
